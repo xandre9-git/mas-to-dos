257b9b9c59b80667c8f0d3e79a163fab1ab80069
xandre9-git <jmjames673@gmail.com>
2023-05-04 20:15:56 -0400

    Create top bar and top bar title

--- src/dom.js
+++ src/dom.js
@@ -1,2 +1,17 @@
+// DOM elements
+
+// main container for the app
 const body = document.getElementById("content");
+
+// top bar
+const topBar = document.createElement("div");
+topBar.id = "top-bar";
+
+const topBarTitle = document.createElement("h1");
+topBarTitle.textContent = "Mas To Do's";
+
+topBar.appendChild(topBarTitle);
+body.appendChild(topBar);
+
+
 export { body };

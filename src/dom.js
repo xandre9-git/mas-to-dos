d698d83534d064e22ad91d0932f34d600e6efb11
xandre9-git <jmjames673@gmail.com>
2023-05-06 22:36:10 -0400

    Create id for title bar, create left side bar div with repsective id, create to do boards container div and respective id and append new dom elements

--- src/dom.js
+++ src/dom.js
@@ -10,10 +10,21 @@ topBar.id = "top-bar";
 // top bar title
 const topBarTitle = document.createElement("h1");
 topBarTitle.textContent = "Mas To Do's";
+topBarTitle.id = "title-bar"
 topBar.appendChild(topBarTitle);
+
+// left side bar
+const leftSideBar = document.createElement("div");
+leftSideBar.id = "left-side-bar";
+
+// to do board
+const todoBoardsContainer = document.createElement("div");
+todoBoardsContainer.id = "to-dos-container";
 
 // dom appends
 body.appendChild(topBar);
+body.appendChild(leftSideBar);
+body.appendChild(todoBoardsContainer);
 
 
 export { body };

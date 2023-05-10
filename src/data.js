69ada503f81c114607662f89195700492f01e8ba
xandre9-git <jmjames673@gmail.com>
2023-05-10 19:00:35 -0400

    Create variable to hold projects and tasks which will be used throughout app

---
+++ src/data.js
@@ -0,0 +1,10 @@
+// --- APP DATA ---
+
+// PROJECTs AND TASKS STORAGE
+const projectsAndTasks = [
+  { projectName: "My Tasks", currentTasks: [], completedTasks: [] },
+];
+
+// export
+
+export { projectsAndTasks };

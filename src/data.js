0dbdf57dd98c320446e0542c3b08dff77d92d56f
xandre9-git <jmjames673@gmail.com>
2023-05-12 18:00:56 -0400

    Add additional element to array for testing

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@
 
 // PROJECTs AND TASKS STORAGE
 const projectsAndTasks = [
-  { projectName: "My Tasks", currentTasks: [], completedTasks: [] }, 
+  { projectName: "My Tasks", currentTasks: [], completedTasks: [] }, { projectName: "Odin", currentTasks: [], completedTasks: [] }
 ];
 
 // export

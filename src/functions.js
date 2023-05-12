3edaf30f97b94ec55bb3b400783d60e7aeeffdaa
xandre9-git <jmjames673@gmail.com>
2023-05-11 20:38:52 -0400

    Create conditional statement to check if project name is not an empty string or already exists in database. Add new project to database if these requirements are met.

--- src/functions.js
+++ src/functions.js
@@ -4,11 +4,17 @@ import { projectsAndTasks } from "./data";
 function projectCreator(){
   // prompt user for project name
   const projectName = prompt("Enter project name:");
-  // take project name and create new entry, if project name is empty string or an existing project, cancel creation
-  if (projectName != "" && projectsAndTasks.includes(projectName)) {
+  // take project name and check if project name is empty string or an existing project
+  // .some() is used on array to search if projectName already exists
+  console.log(projectsAndTasks);
+  if (projectName != "" && !projectsAndTasks.some(e => e.projectName === projectName)) {
     console.log("Criteria met.");
+    // now create a new object in the database with the new project name
+    projectsAndTasks.push({"projectName": projectName, currentTasks: [], completedTasks: []})
+    console.log(projectsAndTasks);
   } else {
     console.log("Criteria not met.");
+    return;
   }
 
 }

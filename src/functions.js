6284a4edfeeca8469b61ccb19ef54721443b81a4
xandre9-git <jmjames673@gmail.com>
2023-05-11 21:08:31 -0400

    Update projectCreator() to add new projects to database, format code

--- src/functions.js
+++ src/functions.js
@@ -1,22 +1,28 @@
 // --- APP FUNCTIONS ---
 import { projectsAndTasks } from "./data";
 
-function projectCreator(){
+function projectCreator() {
   // prompt user for project name
   const projectName = prompt("Enter project name:");
   // take project name and check if project name is empty string or an existing project
   // .some() is used on array to search if projectName already exists
   console.log(projectsAndTasks);
-  if (projectName != "" && !projectsAndTasks.some(e => e.projectName === projectName)) {
+  if (
+    projectName != "" &&
+    !projectsAndTasks.some((e) => e.projectName === projectName)
+  ) {
     console.log("Criteria met.");
     // now create a new object in the database with the new project name
-    projectsAndTasks.push({"projectName": projectName, currentTasks: [], completedTasks: []})
+    projectsAndTasks.push({
+      projectName: projectName,
+      currentTasks: [],
+      completedTasks: [],
+    });
     console.log(projectsAndTasks);
   } else {
     console.log("Criteria not met.");
     return;
   }
-
 }
 
-export { projectCreator }
+export { projectCreator };

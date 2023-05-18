bd2232f205593ba473a41b905bbe29a11418b203
xandre9-git <jmjames673@gmail.com>
2023-05-18 18:38:24 -0400

    Import currentTaskList DOM element for use, format code, add event listener to projects clicked on which displays respective tasks, and finalize work on displayTasks() (function now gets project name and displays tasks specific to it).

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,6 @@
 // --- APP FUNCTIONS ---
 import { projectsAndTasks } from "./data";
+import { currentTaskList } from "./dom";
 
 // creates projects in database
 function projectCreator() {
@@ -32,7 +33,7 @@ let prevProject = null;
 // function to switch which project is active
 const projectClicked = (e) => {
   // check to see if element clicked is a <li> (list item)
-  if (e.target.nodeName === 'LI'){
+  if (e.target.nodeName === "LI") {
     // add .active class to element clicked
     e.target.classList.add("active");
     // check to see if there is a previous project that was active
@@ -42,8 +43,8 @@ const projectClicked = (e) => {
     }
     // set prevProject as the clicked HTMLIElement
     prevProject = e.target;
-  }  
-}
+  }
+};
 
 // displays projects onto the DOM
 function displayProjects(arr, parentNode) {
@@ -56,23 +57,34 @@ function displayProjects(arr, parentNode) {
     // set the text content to the project name found in this iteration
     addProject.textContent = arr[i].projectName;
     // create conditional to check if default task is matched
-    if (arr[i].projectName === "My Tasks"){
-      // set default project to active 
+    if (arr[i].projectName === "My Tasks") {
+      // set default project to active
       addProject.classList.add("active");
       // set variable that monitors active projects to default project
       prevProject = addProject;
     }
+    // add event listener to include tasks for the project
+    addProject.addEventListener("click", function(e){
+      console.log(e.target.textContent);
+      let project = e.target.textContent;
+      currentTaskList.innerHTML = "";
+      displayTasks(project, projectsAndTasks, currentTaskList)
+    });
     // append newly added project to specified node
     parentNode.appendChild(addProject);
   }
 }
 
 // creates tasks
-function taskCreator(task){
+function taskCreator(task) {
   // create variable to search database array for project that is currently active
-  const projectExists = projectsAndTasks.some((e) => e.projectName === prevProject.textContent);
+  const projectExists = projectsAndTasks.some(
+    (e) => e.projectName === prevProject.textContent
+  );
   // find index of project found in database
-  const projectIndex = projectsAndTasks.findIndex((e) => e.projectName === prevProject.textContent);
+  const projectIndex = projectsAndTasks.findIndex(
+    (e) => e.projectName === prevProject.textContent
+  );
   // create newTask object to later push to projectsAndTasks array
   let newTask = {
     // id: newId,
@@ -85,33 +97,45 @@ function taskCreator(task){
   // push newly created task to correct index of projectsAndTasks in the currentTasks property
   projectsAndTasks[projectIndex].currentTasks.push(newTask);
   // save to localStorage to keep data after page reloads
-  return localStorage.setItem("projectsAndTasks", JSON.stringify(projectsAndTasks));
+  return localStorage.setItem(
+    "projectsAndTasks",
+    JSON.stringify(projectsAndTasks)
+  );
 }
 
-function displayTasks(arr, parentNode) {
+function displayTasks(project, arr, parentNode) {
+  // first find the project name in the array
+  const projectIndex = projectsAndTasks.findIndex(
+    (e) => e.projectName === project
+  );
 
-  for (let i = 0; i < arr.length; i++) {
-    console.log(arr.length);
-    console.log(arr);
+  const tasks = [];
+
+  Object.values(arr[projectIndex].currentTasks).forEach((e) => tasks.push(e.task));
+
+  console.log(tasks);
+
+  for (let i = 0; i < tasks.length; i++) {
+
+
     const taskContainer = document.createElement("div");
     taskContainer.className = "task-list-item-container";
-  
+
     const listItem = document.createElement("li");
     listItem.className = "task-list-item";
-    listItem.textContent = arr[i].currentTasks;
+    listItem.textContent = tasks[i];
     taskContainer.appendChild(listItem);
-  
+
     const editBtn = document.createElement("div");
     editBtn.className = "task-edit-btn";
     taskContainer.appendChild(editBtn);
-  
+
     const completeBtn = document.createElement("div");
     completeBtn.className = "task-complete-btn";
     taskContainer.appendChild(completeBtn);
 
     parentNode.appendChild(taskContainer);
   }
-  
 }
 
 // exports

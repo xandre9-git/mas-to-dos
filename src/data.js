a08986b0aa708dc4997887e11c0fe66215b94f22
xandre9-git <jmjames673@gmail.com>
2023-05-24 20:29:33 -0400

    Rename variables for more description

--- src/data.js
+++ src/data.js
@@ -1,10 +1,12 @@
 // --- APP DATA ---
 
 // PROJECTs AND TASKS STORAGE
-const localStorage =  window.localStorage.getItem("projectsAndTasks");
-const projectsAndTasks = (localStorage != null) ? JSON.parse(localStorage) : [
+const projectsAndTaskslocalStorage =  window.localStorage.getItem("projectsAndTasks");
+const projectsAndTasks = (projectsAndTaskslocalStorage != null) ? JSON.parse(projectsAndTaskslocalStorage) : [
   { projectName: "My Tasks", currentTasks: [], completedTasks: [] }, { projectName: "Odin", currentTasks: [], completedTasks: [] }
 ];
+
+
 
 // exports
 export { projectsAndTasks };

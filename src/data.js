442bc176d302f4716e7ba6c168ae1828d85002f4
xandre9-git <jmjames673@gmail.com>
2023-06-14 18:35:10 -0400

    Format code

--- src/data.js
+++ src/data.js
@@ -1,12 +1,10 @@
 // --- APP DATA ---
 
 // PROJECTs AND TASKS STORAGE
-const projectsAndTaskslocalStorage =  window.localStorage.getItem("projectsAndTasks");
-const projectsAndTasks = (projectsAndTaskslocalStorage != null) ? JSON.parse(projectsAndTaskslocalStorage) : [
+const localStorage =  window.localStorage.getItem("projectsAndTasks");
+const projectsAndTasks = (localStorage != null) ? JSON.parse(localStorage) : [
   { projectName: "My Tasks", currentTasks: [], completedTasks: [] }, { projectName: "Odin", currentTasks: [], completedTasks: [] }
 ];
-
-
 
 // exports
 export { projectsAndTasks };
